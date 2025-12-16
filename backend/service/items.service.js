a69ec2b583c31350c69a0dc8f727d9073ed4bd79
andriyko66 <147740137+andriyko66@users.noreply.github.com>
2025-12-17 01:26:12 +0200

    Update items.service.js

--- backend/service/items.service.js
+++ backend/service/items.service.js
@@ -7,21 +7,42 @@ function getAll() {
   return items;
 }
 
-function getById(id) {
-  return items.find(i => i.id === id);
-}
-
 function create(data) {
+  if (!data.name) {
+    throw new Error("NAME_REQUIRED");
+  }
+
   const item = new Item(nextId++, data.name);
   items.push(item);
   return item;
 }
 
+function update(id, data) {
+  const item = items.find(i => i.id === id);
+  if (!item) {
+    return null;
+  }
+
+  if (data.name) {
+    item.name = data.name;
+  }
+
+  return item;
+}
+
 function remove(id) {
   const index = items.findIndex(i => i.id === id);
-  if (index === -1) return false;
+  if (index === -1) {
+    return false;
+  }
+
   items.splice(index, 1);
   return true;
 }
 
-module.exports = { getAll, getById, create, remove };
+module.exports = {
+  getAll,
+  create,
+  update,
+  remove
+};
