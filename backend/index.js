9748ca17b1247515f50d87a075255523db37a193
andriyko66 <147740137+andriyko66@users.noreply.github.com>
2025-12-26 23:41:50 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const itemsRoutes = require("./api/items.routes");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.send("ok");
-});
+// Health check
+app.get("/health", (req, res) => res.json({ status: "ok" }));
 
+// API для товарів
 app.use("/items", itemsRoutes);
+
+// Статичні файли фронтенду
+app.use(express.static("../frontend"));
 
 app.listen(3000, () => {
   console.log("✅ Server running on http://localhost:3000");
